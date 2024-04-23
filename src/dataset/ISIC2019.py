4c04811dde8f5fda10c81d1261f11ae77066f392
lup1n <780966523@qq.com>
2024-04-23 23:31:10 +0200

    :bug: ISIC2019.py

--- src/dataset/ISIC2019.py
+++ src/dataset/ISIC2019.py
@@ -1,3 +1,8 @@
+import sys
+import os
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
 from os import PathLike
 import torch
 from torch.utils.data import Dataset, DataLoader 
