1d024992912ad0ecf66bd45dfcff669022a6290d
lup1n <780966523@qq.com>
2024-04-24 02:09:52 +0200

    :bug:

--- script/train_vit_base.sh
+++ script/train_vit_base.sh
@@ -2,7 +2,7 @@
 PROJECT_ROOT=".."
 
 python ${PROJECT_ROOT}/src/trainer/vit_base_trainer.py \
---processor_config ${PROJECT_ROOT}/config/vit_image_processor.json \
+--8processor_config ${PROJECT_ROOT}/config/vit_image_processor.json \
 --vit_config ${PROJECT_ROOT}/config/vit_isic2019.json \
 --metadata ${PROJECT_ROOT}/data/metadataTrain.csv \
 --ckpt_dir ${PROJECT_ROOT}/ckpt/ \
