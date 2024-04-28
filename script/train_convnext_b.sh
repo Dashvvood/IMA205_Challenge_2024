27813035349cb11a43a140068e6cffc1a24cc232
lup1n <780966523@qq.com>
2024-04-28 21:10:34 +0200

    :bug:

--- script/train_convnext_b.sh
+++ script/train_convnext_b.sh
@@ -3,10 +3,9 @@ PROJECT_ROOT=".."
 
 python ${PROJECT_ROOT}/src/trainer/convnext_s_trainer.py \
 --processor_config ${PROJECT_ROOT}/config/convnext_processor.json \
---model_config ${PROJECT_ROOT}/config/convnext_s.json \
---metadata ${PROJECT_ROOT}/data/ISIC2019/metadataTrain.csv \
+--model_config ${PROJECT_ROOT}/config/convnext_b.json \
+--metadata ${PROJECT_ROOT}/data/ISIC2019_medaug/metadataMedAugTrain.csv \
 --ckpt_dir ${PROJECT_ROOT}/ckpt/ \
 --log_dir ${PROJECT_ROOT}/log/ \
---img_root ${PROJECT_ROOT}/data/ISIC2019/Train/Train \
+--img_root ${PROJECT_ROOT}/data/ISIC2019_medaug/Train/ \
 $@
-
