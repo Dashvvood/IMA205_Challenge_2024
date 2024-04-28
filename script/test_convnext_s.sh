d7776dd20f80a5f0bf55f01fc25df5ad38d56e29
lup1n <780966523@qq.com>
2024-04-28 22:20:37 +0200

    :bug:

--- script/test_convnext_s.sh
+++ script/test_convnext_s.sh
@@ -4,7 +4,7 @@ PROJECT_ROOT=".."
 python ${PROJECT_ROOT}/src/tester/convnext_tester.py \
 --processor_config ${PROJECT_ROOT}/config/convnext_processor.json \
 --vit_config ${PROJECT_ROOT}/config/convnext_s.json \
---metadata ${PROJECT_ROOT}/data/metadataTest.csv \
+--metadata ${PROJECT_ROOT}/data/ISIC2019/metadataTest.csv \
 --ckpt_dir ${PROJECT_ROOT}/ckpt/ \
 --log_dir ${PROJECT_ROOT}/log/ \
 --img_root ${PROJECT_ROOT}/data/Test/Test \
