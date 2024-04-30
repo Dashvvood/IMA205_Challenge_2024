58b6ed6f596b08e601653d2965247b4a99761a34
lup1n <780966523@qq.com>
2024-04-30 08:39:21 +0200

    :bug:

--- script/test_convnext_b.sh
+++ script/test_convnext_b.sh
@@ -3,7 +3,7 @@ PROJECT_ROOT=".."
 
 python ${PROJECT_ROOT}/src/tester/convnext_tester.py \
 --processor_config ${PROJECT_ROOT}/config/convnext_processor.json \
---model_config ${PROJECT_ROOT}/config/convnext_s.json \
+--model_config ${PROJECT_ROOT}/config/convnext_b.json \
 --metadata ${PROJECT_ROOT}/data/ISIC2019/metadataTest.csv \
 --img_root ${PROJECT_ROOT}/data/ISIC2019/Test/Test \
 $@
--- script/test_vit_base.sh
+++ script/test_vit_base.sh
@@ -4,8 +4,6 @@ PROJECT_ROOT=".."
 python ${PROJECT_ROOT}/src/tester/vit_base_tester.py \
 --processor_config ${PROJECT_ROOT}/config/vit_image_processor.json \
 --vit_config ${PROJECT_ROOT}/config/vit_isic2019.json \
---metadata ${PROJECT_ROOT}/data/metadataTest.csv \
---ckpt_dir ${PROJECT_ROOT}/ckpt/ \
---log_dir ${PROJECT_ROOT}/log/ \
---img_root ${PROJECT_ROOT}/data/Test/Test \
+--metadata ${PROJECT_ROOT}/data/ISIC2019/metadataTest.csv \
+--img_root ${PROJECT_ROOT}/data/ISIC2019/Test/Test \
 $@
